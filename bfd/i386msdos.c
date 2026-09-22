a35220bf1ae13e729ed2193ca059c9bd1d6650dc
Qwinci <qwinci222@gmail.com>
2026-09-22 19:13:44 +0300

    bfd/i386msdos: lower priority

--- bfd/i386msdos.c
+++ bfd/i386msdos.c
@@ -277,7 +277,7 @@ const bfd_target i386_msdos_vec =
     0,				/* leading underscore */
     ' ',				/* ar_pad_char */
     16,				/* ar_max_namelen */
-    0,				/* match priority.  */
+    1,				/* match priority.  */
     TARGET_KEEP_UNUSED_SECTION_SYMBOLS, /* keep unused section symbols.  */
     bfd_getl64, bfd_getl_signed_64, bfd_putl64,
     bfd_getl32, bfd_getl_signed_32, bfd_putl32,
